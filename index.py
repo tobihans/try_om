a14bbc0086118c4de39eaa930b6c10f2f321492d
tobihans <learningway403@gmail.com>
2022-11-08 13:38:26 +0100

    type coerce

--- index.py
+++ index.py
@@ -7,7 +7,10 @@ class Language(HashModel):
     creation_date: date
     nb_ob_keywords: Optional[int]
 
-python = Language(author="Guido van Rossum", creation_date=date(year=1991, month=1, day=1), nb_of_keywords = 38)
-rust = Language(author="Graydon Hoare", creation_date=date(year=2010, month=1, day=1))
-typescript = Language(author="Anders Hejlsberg", creation_date=date(year=2012, month=1, day=1))
-nim = Language(author="Andreas Rumpf", creation_date=date(year=2008, month=1, day=1))
+python = Language(author="Guido van Rossum", creation_date="1991", nb_of_keywords = 38)
+rust = Language(author="Graydon Hoare", creation_date="2010")
+typescript = Language(author="Anders Hejlsberg", creation_date="2012")
+nim = Language(author="Andreas Rumpf", creation_date="2008")
+
+print(nim.pk)
+print(nim.creation_date, type(nim.creation_date))
